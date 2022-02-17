8408a1fc621f282c0f3bc8d209b97d667e2c97e7
Md Nazmul Hasan <nazmul.ns7989@gmail.com>
2022-02-17 18:10:28 +0600

    react set up done

---
+++ resources/js/router/AppRoute.js
@@ -0,0 +1,21 @@
+import React, {Component, Fragment} from 'react';
+import { Routes, Route} from "react-router-dom";
+import Home from "../components/Home";
+
+
+class AppRoute extends Component {
+    render() {
+        return (
+            <Fragment>
+
+                <Routes>
+                    <Route path="/" element={<Home />} />
+
+                </Routes>
+
+            </Fragment>
+        );
+    }
+}
+
+export default AppRoute;
